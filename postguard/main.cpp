21455a2c8ae1b2daac95b7172322af43f5aa3f70
Cody Cutrer <cody@instructure.com>
2014-01-23 09:42:54 -0700

    Initial commit

---
+++ postguard/main.cpp
@@ -0,0 +1,42 @@
+// Copyright (c) 2013 - Cody Cutrer
+
+#include "mordor/predef.h"
+
+#include <iostream>
+
+#include <mordor/config.h>
+#include <mordor/daemon.h>
+#include <mordor/iomanager.h>
+#include <mordor/main.h>
+#include <mordor/streams/ssl.h>
+
+using namespace Mordor;
+
+namespace Postguard {
+
+static int daemonMain(int argc, char *argv[])
+{
+    try {
+        IOManager ioManager;
+        boost::shared_ptr<SSL_CTX> sslCtx(SSLStream::generateSelfSignedCertificate());
+
+        ioManager.stop();
+        return 0;
+    } catch (...) {
+        std::cerr << boost::current_exception_diagnostic_information() << std::endl;
+        return -1;
+    }
+}
+
+}
+
+MORDOR_MAIN(int argc, char *argv[])
+{
+    try {
+        Config::loadFromEnvironment();
+        return Daemon::run(argc, argv, &Postguard::daemonMain);
+    } catch (...) {
+        std::cerr << boost::current_exception_diagnostic_information() << std::endl;
+        return -1;
+    }
+}
